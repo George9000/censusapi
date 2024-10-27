d5fe1a753703a209c41e3bf030c36768dfc18ebb
George9000 <thomaspg976@gmail.com>
2024-10-26 23:19:46 -0400

    Data obtained and imported into dataframe.
    Also, license, Chain, user guide and documentation.

--- flows.jl
+++ flows.jl
@@ -3,4 +3,6 @@ using censusapi
 # const censustoken = key_as_string_here
 
 # example query
-getcensus("2022/acs/flows", ["get" => "FULL1_NAME,FULL2_NAME,GEOID1,GEOID2,MOVEDIN", "for" => "county:*", "in" => "state:*", "key" => "$censustoken"], "2022_migration_flows.txt")
+getcensus("2022/acs/flows", ["get" => "STATE1,STATE1_NAME,FULL1_NAME,FULL2_NAME,GEOID1,GEOID2,MOVEDIN,NONMOVERS,POP1YRAGO,POP1YR", "for" => "county:*", "in" => "state:*", "key" => "$censustoken"], "2022_flows.txt")
+
+const flows = flowdf("2022_flows.txt")
--- src/censusapi.jl
+++ src/censusapi.jl
@@ -1,7 +1,7 @@
 module censusapi
 using HTTP, JSON3, ExpandNestedData
 using DataFrames, AnalysisUtils, CSV, Missings
-export getcensus
+export getcensus, flowdf
 
 """
     getcensus(dataseturl, query, datafilepath)
@@ -12,10 +12,24 @@ dataseturl, string, subpath to append to base url; no trailing slash
 query, Vector{Pair}, query as a vector of key => value string pairs
 """
 function getcensus(dataseturl, query, datafilepath)
-	io = open(datafilepath, "w")
-	response = HTTP.get("https://api.census.gov/data/"*dataseturl, query = query, response_stream = io)
-	close(io)
-	print(response.status)
+    io = open(datafilepath, "w")
+    response = HTTP.get("https://api.census.gov/data/"*dataseturl, query = query, response_stream = io)
+    close(io)
+    print(response.status)
 end
+
+"""
+    flowdf(filename::String)
+
+Construct a census flows dataframe
+from the textfile returned by the getcensus function.
+"""
+function flowdf(filename::String)
+    strexpr = read(filename, String)
+    strexpr = replace(strexpr, r"\"" => "", "[" => "", "]," => "", "]]" => "", "\\u00F1" => "ñ")
+    df = CSV.read(IOBuffer(strexpr), DataFrame; missingstring = "null")
+end
+# vec = eval(Meta.parse(strexpr))
+# return DataFrame(Tuple.(vec[2:end]), vec[1])
 
 end # module censusapi
